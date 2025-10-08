308433d2982a74d1cd95ec143d0562f5b967985d
Isuru Dulaj <isurudulaj100@gmail.com>
2025-10-08 12:11:16 +0530

    user controller update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { saveUser } from "../controllers/userController.js";
+import { googleLogin, saveUser } from "../controllers/userController.js";
 import { loginUser } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/",saveUser);
 userRouter.post("/login",loginUser);
+userRouter.post("/google",googleLogin);
 
 export default userRouter;
